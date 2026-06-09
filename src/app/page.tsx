b8f6f874bd04dd9b997153389f5d7d1d52811927
jesustissera <jesustissera@gmail.com>
2026-06-09 17:09:25 -0300

    tarea (hero): agregar noticias destacadas en hero.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Hero } from "@/components/features/home/Hero/Hero";
 import { LatestNews } from "@/components/features/home/LatestNews/LatestNews";
 import { getLatestNews } from "@/services/news";
+import { getFeaturedNews } from "@/services/featured_news";
 
 export default async function Home() {
   const news = await getLatestNews();
+  const featuredNews = await getFeaturedNews();
+
   return (
     <>
-      <Hero slides={news.slice(0, 3)} />
+      <Hero slides={featuredNews} />
       <LatestNews news={news.slice(0, 3)} />
     </>
   
