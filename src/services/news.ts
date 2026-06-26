17f83669475765acb66a3f03f30af486f9574833
jesustissera <jesustissera@gmail.com>
2026-06-26 09:31:31 -0300

    refactor (news): funcionalidad para filtrar por categoría

--- src/services/news.ts
+++ src/services/news.ts
@@ -14,3 +14,20 @@ export async function getLatestNews() {
     return [];
   }
 }
+
+export async function getRelatedNewsByCategory(slug: string) {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/news?filters[categories][slug][$eq]=${slug}&populate=*&sort=id:desc`,
+    );
+    if (!res.ok) {
+      console.error("Error API noticias:", res.status);
+      return [];
+    }
+    const data = await res.json();
+    return data.data;
+  } catch (err) {
+    console.error("Error fetching news:", err);
+    return [];
+  }
+}
