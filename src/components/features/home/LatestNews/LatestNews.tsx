6318cb8d64df9f410b2e518589e7039ad4cb3598
jesustissera <jesustissera@gmail.com>
2026-07-02 09:31:20 -0300

    Tarea (news): incorporo botón para ver más noticias

--- src/components/features/home/LatestNews/LatestNews.tsx
+++ src/components/features/home/LatestNews/LatestNews.tsx
@@ -1,12 +1,12 @@
 import { CardNew } from "@/components/ui/CardNew/CardNew";
+import { ButtonLink } from "@/components/ui/buttonLink/ButtonLink";
 import { News } from "@/types/news";
 
 export interface NewsProps {
   news: News[];
 }
 
-export const LatestNews = async({ news }: NewsProps) => {
-
+export const LatestNews = async ({ news }: NewsProps) => {
   return (
     <section className="w-full py-20">
       <div className="max-w-7xl mx-auto px-6">
@@ -17,6 +17,25 @@ export const LatestNews = async({ news }: NewsProps) => {
           {news.map((item) => (
             <CardNew key={item.id} new={item} />
           ))}
+        </div>
+        <div className="flex justify-center mt-5">
+          <ButtonLink
+            href="/noticias"
+            bgColor="bg-black"
+            hoverBgColor="hover:bg-white"
+            textColor="text-white"
+            hoverTextColor="hover:text-black"
+            borderColor="border border-[#000000] border-2"
+            hoverBorderColor="hover:border-[#000000] hover:border-2"
+            target="_self"
+            eventData={{
+              event: "contact_click",
+              method: "letter_open",
+              location: "home",
+            }}
+          >
+            Ver más →
+          </ButtonLink>
         </div>
       </div>
     </section>
