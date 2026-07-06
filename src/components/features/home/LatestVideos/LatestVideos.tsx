57f7ce4980a00d9e7f22d9375d721ee58fa2da2a
jesustissera <jesustissera@gmail.com>
2026-07-05 21:29:35 -0300

    Refactor: agrego botón para acceder al canal de Youtube

--- src/components/features/home/LatestVideos/LatestVideos.tsx
+++ src/components/features/home/LatestVideos/LatestVideos.tsx
@@ -1,5 +1,6 @@
 import { YoutubeVideo } from "@/types/youtube";
 import { YoutubeCard } from "@/components/ui/YouTubeCard/YouTubeCard";
+import { ButtonLink } from "@/components/ui/buttonLink/ButtonLink";
 
 type Props = {
   videos: YoutubeVideo[];
@@ -15,6 +16,25 @@ export const LatestVideos = ({ videos }: Props) => {
           {videos.map((video) => (
             <YoutubeCard key={video.id} video={video} />
           ))}
+        </div>
+        <div className="flex justify-center mt-5">
+          <ButtonLink
+            href="https://www.youtube.com/@LaCicloneta"
+            bgColor="bg-black"
+            hoverBgColor="hover:bg-white"
+            textColor="text-white"
+            hoverTextColor="hover:text-black"
+            borderColor="border border-[#000000] border-2"
+            hoverBorderColor="hover:border-[#000000] hover:border-2"
+            target="_blank"
+            eventData={{
+              event: "contact_click",
+              method: "letter_open",
+              location: "home",
+            }}
+          >
+            Ir al canal →
+          </ButtonLink>
         </div>
       </div>
     </section>
